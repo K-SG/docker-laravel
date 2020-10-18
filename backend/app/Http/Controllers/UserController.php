e6fbf871620bb7d8d2d6d896203a00100d292abc
SugiKoki <fnvfm00957@yahoo.co.jp>
2020-10-18 23:29:22 +0900

    MVCでやってみた

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use App\Models\K_User;
+
 class UserController extends Controller
 {
+
     public function usernew(){
+        // $show_user_all = K_User::all();
+        // dd($show_user_all);
         $popFlag = 7;
-        
         return view('user.user_new',['popFlag'=>$popFlag]);
     }
     public function usercreate(Request $request){
@@ -23,10 +27,10 @@ class UserController extends Controller
         
         //入力されてきたメールを条件にユーザーをselect
         //userCount = SELECT COUNT(*) FROM k_user WHERE mail = ?
-        $bookingUser = DB::select('select * from k_user where mail = :mail', $mailparam);
-        
-        // dd($booking_usercount);
-        if($bookingUser == null){
+        // $bookingUser = DB::select('select * from k_user where mail = :mail', $mailparam);
+        // $bookingUser = DB::table('k_user')->where('mail',$mailparam);
+        $booking_user = K_User::all()->where('mail',$mailparam);
+        if($booking_user != null){
             $popFlag = 1;
             return view('user.user_new',['popFlag'=> $popFlag]);
         }
