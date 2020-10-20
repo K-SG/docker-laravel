05aec6e67da49aaf2e262094fd6fb8f33764f5d8
SugiKoki <fnvfm00957@yahoo.co.jp>
2020-10-20 18:01:11 +0900

    ユーザー登録機能が完成しました。

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -16,31 +16,23 @@ class UserController extends Controller
     }
     public function register(Request $request)
     {
+        //登録完了ポップアップを表示するためのフラグ
         $popup_flag = 0;
-        // $users = [
-        //     'user_name' => $request->userName,
-        //     'mail' => $request->mail,
-        //     'password' => $request->password,
-        // ];
-        //データ取得
-        // $booking_user = KrononUser::where('mail',$mail)->get();
         $booking_user = KrononUser::mailEqual($request->mail)->get();
-        // empty($booking_user)isset($booking_user[0])
         if (count($booking_user) != 0) {
-            //メールアドレスと被った際のポップアップを出すためのフラグ
+            //メールアドレスと被った際のポップアップを表示するためのフラグ
             $popup_flag = 1;
             return view('user.user_new', ['popFlag' => $popup_flag]);
         }
-        // DB::table('kronon_users')->insert($users);
         $krononUser = new KrononUser;
-        
+        //値を保存
         $krononUser->user_name = $request->userName;
         $krononUser->mail = $request->mail;
         $krononUser->password = $request->password;
+        //タイムスタンプを無効
         $krononUser->timestamps = false; 
         unset($krononUser['_token']);
         $krononUser->save();
-        
         return view('user.user_new', ['popFlag' => $popup_flag]);
     }
 }
