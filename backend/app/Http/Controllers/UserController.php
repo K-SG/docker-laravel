27f30ac17f2dd318b1bda2254fbdfa7f38910440
SugiKoki <fnvfm00957@yahoo.co.jp>
2020-10-22 15:25:57 +0900

    Authをコメントアウト

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller
         //登録完了ポップアップを表示するためのフラグ
         $popup_flag = 0;
         //バリデーション$request->mailformrequest
+        
         $booking_user = KrononUser::SelectByMail($request->mail)->get();
         if (count($booking_user) != 0) {
             //メールアドレスと被った際のポップアップを表示するためのフラグ
