5ec14ff86a8d6d581e1c3b7758686905df7f5b6a
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-10-24 17:39:51 +0900

    Userをインサートできるようになったが、そのあとのリダイレクト先がhomeに

--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -20,9 +20,9 @@
 				<input type="text" name="name" id="create_name" class="create_textbox" placeholder="表示名" maxlength="15" value={{old('name')}}><br>
 				<input type="text" name="email" id="create_mail" class="create_textbox" placeholder="メールアドレス" maxlength="100" value={{old('email')}}><br>
 				<input type="password" name="password" id="create_password1" class="create_textbox" placeholder="パスワード" maxlength="20" value={{old('password')}}><br>
-				<input type="password" name="password" id="create_password2" class="create_textbox" placeholder="パスワード確認" maxlength="20" value={{old('password')}}><br>
-				<input type="hidden" id="flag" value="{{$popFlag}}"> <input type="button" class="user-create-button" value="新規登録">
-				
+				<input type="password" name="password_confirmation" id="create_password2" class="create_textbox" placeholder="パスワード確認" maxlength="20" value={{old('password')}}><br>
+				<input type="hidden" id="flag" value="{{$popFlag}}"> 
+				<input type="button" class="user-create-button" value="新規登録">
 				<!--エラーポップアップ------------------------------------------------------------------->
 				<div class="popup-wrapper error-popup create-popup">
 					<div class="pop-container">
