cf6a793b0e40d0a48f662a4b2064bcd87b8cc4f7
h-okuno <hirokki.oku@gmail.com>
2020-10-26 14:28:12 +0900

    ユーザー認証追加

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\KrononUser;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function topPage()
     {
-        return view('login.login');
+        $param = ['message' => 'ログイン'];
+        return view('login.login', $param);
     }
 
     public function login(Request $request)
     {
-        $mail = $request->mail;
+        $email = $request->email;
         $password = $request->password;
 //        $items = KrononUser::LoginCheck($mail, $password)->get();
 
@@ -26,14 +28,12 @@ class LoginController extends Controller
         //     return redirect('/user/calendar');
         // }
 
-        if (Auth::attempt(['email' => $mail,
-                            'password' => $password])) 
+        if (Auth::attempt(['email' => $email, 'password' => $password])) 
         {
             return redirect('/user/calendar');
         } else {
             $msg = 'ログインに失敗しました。';
-            $popFlag = 2;
-            return view('login.login', ['popFlag' => $popFlag]);
+            return view('login.login', ['message' => $msg]);
         }
     
     }
