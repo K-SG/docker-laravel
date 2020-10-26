b634eae3cf6adb528127ba12a2b3058789d53a46
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-10-26 18:31:30 +0900

    registerできるようになりました。user_newをメソッドに分けてます。

--- backend/public/js/user_new.js
+++ backend/public/js/user_new.js
@@ -1,85 +1,122 @@
-"use strict"
-
-$(function () {
-
-	/*DBと照合した後のエラーポップアップ表示*/
-	$(document).ready(function () {
-		let popFlag = document.getElementById('flag').value;
-		  /*登録が完了した場合*/
-		  if(popFlag == 0){
-			  $('.create-msg').html('登録が完了したよ！');
-			  $('.complete-popup').fadeIn();
-			  return;
-		  }
-		  /*メールアドレスがすでに登録されていた場合*/
-		  if(popFlag == 1){
-			  $('.create-popup').fadeIn();
-			  return;
-		  }
-	});
-
-	/*新規登録ボタンを押下した際のエラーチェックとポップアップ表示*/
-	$('.user-create-button').click(function () {
-		let popFlag = document.getElementById('flag').value;
-		let userName = document.getElementById('create_name').value;
-	    let mail = document.getElementById('create_mail').value;
-	    let password1 = document.getElementById('create_password1').value;
-	    let password2 = document.getElementById('create_password2').value;
-
-	    if(userName.trim() == '' || mail == '' || password1 == '' || password2 == ''){
-	    	$('.create-msg').html('入力されていない<br>項目があるよ！');
-	        $('.create-popup').fadeIn();
-	        return;
-	    }else if(!mail.match(/^[A-Za-z0-9]+[\w\-_]+@[\w\._]+\.\w{2,}$/)){
-			$('.create-msg').html('メールアドレスが<br>不正だよ！');
-	        $('.create-popup').fadeIn();
-	        return;
-        }else if(password1.length < 8 || !(password1.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)&& password1.match(/([0-9])/)) || !(password1.match(/([a-zA-Z])/) && password1.match(/([0-9])/))){
-			$('.create-msg').html('パスワードの条件を<br>満たしていないよ！');
-	        $('.create-popup').fadeIn();
-	        return;
-	    }else if(password1 !== password2){
-			$('.create-msg').html('パスワードが<br>一致していないよ！');
-	        $('.create-popup').fadeIn();
-	        return;
-	    }else{
-	    	popFlag = 0;
-	    }
-	    if(popFlag == 0){
-	        /*表示名の値をセット*/
-	    	let nameText = document.getElementById("confirmUserName");
-			nameText.innerText = document.forms.id_create_form.create_name.value;
-			/*メールの値をセット*/
-			let mailText = document.getElementById("confirmMail");
-			mailText.innerText = document.forms.id_create_form.create_mail.value;
-			/*パスワードの値をセット*/
-			let passwordText = document.getElementById("confirmPassword");
-			/*パスワードの字数を取得*/
-			let tmp = document.forms.id_create_form.create_password1.value.length;
-			var str = '';
-			for (var i = 0; i < tmp; i++) {
-				str += '＊';
-			}
-			passwordText.innerText = str;
-	        $('.confirm-popup').fadeIn();
-	        $('user-create-form').submit();
-	        return;
-	      }
-	  });
-
-	/*ポップアップを閉じる*/
-	$('.close-popup').click(function () {
-		$('.confirm-popup').fadeOut();
-        $('.error-popup').fadeOut();
-        $('.back-popup').fadeOut();
-        $('.create-popup').fadeOut();
-	});
-	/*登録完了ポップアップのOKボタン押下時の遷移先*/
-	$('.next-popup').click(function () {
-        location.href= "user/calendar";
-	});
-	$('#submit-user').click(function(){
-		$('#id_create_form').submit();
-		return;
-	})
+"use strict";
+const POPUP_FLAG_COMPLETED = 1;
+const POPUP_FLAG_MAIL_DUPLICATED = 2;
+const POPUP_FLAG_NONE = 0;
+/*DBと照合した後のエラーポップアップ表示*/
+$(document).ready(function () {
+    /*登録が完了した場合*/
+    if (popFlag == POPUP_FLAG_COMPLETED) {
+        $(".create-msg").html("登録が完了したよ！");
+        $(".complete-popup").fadeIn();
+        return;
+    }
+    /*メールアドレスがすでに登録されていた場合*/
+    if (popFlag == POPUP_FLAG_MAIL_DUPLICATED) {
+        $(".create-popup").fadeIn();
+        return;
+    }
 });
+/*新規登録ボタンを押下した際のエラーチェックとポップアップ表示*/
+$(".user-create-button").click(function () {
+    let userName = document.getElementById("create_name").value;
+    let mail = document.getElementById("create_mail").value;
+    let password1 = document.getElementById("create_password1").value;
+    let password2 = document.getElementById("create_password2").value;
+    let popFlag = document.getElementById("flag").value;
+    if (!isValid()) {
+        return;
+    }
+    showConfirmPopup();
+    function isValidAllRequired() {
+        if (
+            userName.trim() == "" ||
+            mail == "" ||
+            password1 == "" ||
+            password2 == ""
+        ) {
+            return false;
+        }
+        return true;
+    }
+    function isValidMail() {
+        if (mail.match(/^[A-Za-z0-9]+[\w\-_]+@[\w\._]+\.\w{2,}$/)) return false;
+    }
+    function isValidPassword() {
+        if (
+            password1.length < 8 ||
+            !(
+                password1.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/) &&
+                password1.match(/([0-9])/)
+            ) ||
+            !(password1.match(/([a-zA-Z])/) && password1.match(/([0-9])/))
+        )
+            return false;
+    }
+    function isValidPasswordConfirm() {
+        if (password1 != password2) return false;
+    }
+
+    function isValid() {
+        if (!isValidAllRequired()) {
+            $(".create-msg").html("入力されていない<br>項目があるよ！");
+            $(".create-popup").fadeIn();
+            return;
+        }
+        if (!isValidMail()) {
+            $(".create-msg").html("メールアドレスが<br>不正だよ！");
+            $(".create-popup").fadeIn();
+            return;
+        }
+        if (!isValidPassword()) {
+            $(".create-msg").html("パスワードの条件を<br>満たしていないよ！");
+            $(".create-popup").fadeIn();
+            return;
+        }
+        if (!isValidPasswordConfirm) {
+            $(".create-msg").html("パスワードが<br>一致していないよ！");
+            $(".create-popup").fadeIn();
+            return;
+        }
+        return true;
+    }
+});
+
+/*ポップアップを閉じる*/
+$(".close-popup").click(function () {
+    $(".confirm-popup").fadeOut();
+    $(".error-popup").fadeOut();
+    $(".back-popup").fadeOut();
+    $(".create-popup").fadeOut();
+});
+/*登録完了ポップアップのOKボタン押下時の遷移先*/
+$(".next-popup").click(function () {
+    location.href = "user/calendar";
+});
+$("#submit-user").click(function () {
+    $("#id_create_form").submit();
+    return;
+});
+
+function showConfirmPopup() {
+    popFlag = POPUP_FLAG_NONE;
+    if (popFlag === POPUP_FLAG_NONE) {
+        /*表示名の値をセット*/
+        let nameText = document.getElementById("confirmUserName");
+        nameText.innerText = document.forms.id_create_form.create_name.value;
+        /*メールの値をセット*/
+        let mailText = document.getElementById("confirmMail");
+        mailText.innerText = document.forms.id_create_form.create_mail.value;
+        /*パスワードの値をセット*/
+        let passwordText = document.getElementById("confirmPassword");
+        /*パスワードの字数を取得*/
+        let tmp = document.forms.id_create_form.create_password1.value.length;
+        var str = "";
+        for (var i = 0; i < tmp; i++) {
+            str += "＊";
+        }
+        passwordText.innerText = str;
+        $(".confirm-popup").fadeIn();
+        $("user-create-form").submit();
+        return;
+    }
+}
--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
 				<input type="text" name="email" id="create_mail" class="create_textbox" placeholder="メールアドレス" maxlength="100" value={{old('email')}}><br>
 				<input type="password" name="password" id="create_password1" class="create_textbox" placeholder="パスワード" maxlength="20" value={{old('password')}}><br>
 				<input type="password" name="password_confirmation" id="create_password2" class="create_textbox" placeholder="パスワード確認" maxlength="20" value={{old('password')}}><br>
-				<input type="hidden" id="flag" value="{{$popFlag ?? '2'}}"> 
+				<input type="hidden" id="flag" value="{{$popFlag ?? ''}}"> 
 				<input type="button" class="user-create-button" value="新規登録">
 				<!--エラーポップアップ------------------------------------------------------------------->
 				<div class="popup-wrapper error-popup create-popup">
@@ -106,82 +106,3 @@
     <script src="{{ asset('js/common/common.js') }}"></script>
 </body>
 </html>
-
-
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
