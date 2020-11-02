68b3b6870ae0c769a74cb097b80bd67e9184aa2a
h-okuno <hirokki.oku@gmail.com>
2020-11-02 17:46:32 +0900

    レビュー修正

--- backend/app/Http/Controllers/InputScheduleController.php
+++ backend/app/Http/Controllers/InputScheduleController.php
@@ -9,44 +9,37 @@ use Illuminate\Support\Facades\Auth;
 
 class InputScheduleController extends Controller
 {
-    public function inputschedule()
+    public function inputSchedule()
     {
         $user = Auth::user();
 
         return view('schedule.inputschedule');
     }
 
-    public function schedulecreate(Request $request)
+    public function scheduleCreate(Request $request)
     {
-        $user = Auth::user();
-        $user_id = $user->id;
-
-
-        $schedule = new Schedule;
-
         $schedule_date = $request->scheduleDate;
         $start_time = $request->startTimeHour . ":" . $request->startTimeMin . ":" . "00";
         $end_time = $request->endTimeHour . ":" . $request->endTimeMin . ":" . "00";
-
-        //予定が重複していないか確認
-        $param = Schedule::isBooking($schedule_date, $user_id, $start_time, $end_time);
-
-        if (empty($param)) {
-        //データベースに値を送信
-        $schedule->user_id = $user_id;
-        $schedule->schedule_date = $schedule_date;
-        $schedule->start_time = $start_time;
-        $schedule->end_time = $end_time;
-        $schedule->place = $request->place;
-        $schedule->title = $request->title;
-        $schedule->content = $request->content;
-        $schedule->delete_flag = 0;
-        $schedule->save();
-
+        
         $user = Auth::user();
+        $user_id = $user->id;
+        //予定が重複していないか確認
+        $exist = Schedule::isBooking($schedule_date, $user_id, $start_time, $end_time);
 
-        return redirect('/user/calendar');
-            
+        if (empty($exist)) {
+            $schedule = new Schedule;
+            //データベースに値を送信
+            $schedule->user_id = $user_id;
+            $schedule->schedule_date = $schedule_date;
+            $schedule->start_time = $start_time;
+            $schedule->end_time = $end_time;
+            $schedule->place = $request->place;
+            $schedule->title = $request->title;
+            $schedule->content = $request->content;
+            $schedule->delete_flag = 0;
+            $schedule->save();
+            return redirect('/user/calendar');   
         } else {
             return view('schedule.inputschedule');
         }
