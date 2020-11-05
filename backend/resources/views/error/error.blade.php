8b60adafcbe8c93f618e57f8a8ff8ab8d46b97cc
nakane-benesse <nakane.benesse@gmail.com>
2020-11-05 19:55:55 +0900

    エラーページ修正しました

--- backend/resources/views/error/error.blade.php
+++ backend/resources/views/error/error.blade.php
@@ -1,19 +1,15 @@
-@extends('layouts.templateapp')
 
-@section('link')
-    @parent
-    <link href="{{ asset('css/error.css') }}" rel="stylesheet" type="text/css">
-@endsection
-
-@section('title','エラー')
-
-@section('body')
+<html>
+<head>
+  <title>エラー</title>
+  <link href="{{ asset('css/common/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/common/common.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/error.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
     <article>
         <div id="error-msg">問題が発生しちゃったよ。もう一度やり直してみてね。</div>
         <img alt="エラーロゴ" id="error-img" src="{{ asset('img/kronon/kronon_komatta.png') }}">
     </article>
-@endsection
-
-@section('footer')
-
-@section('js_link')
+</body>
+</html>
