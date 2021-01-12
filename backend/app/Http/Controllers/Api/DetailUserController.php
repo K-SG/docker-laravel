6cf0cd76c7dbea6929c22108c7ac597a57fc67a0
h-okuno <hirokki.oku@gmail.com>
2021-01-12 16:40:08 +0900

    作成中

---
+++ backend/app/Http/Controllers/Api/DetailUserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Exceptions\BadRequestException;
+use Exception;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+
+use App\Models\User;
+
+class ScheduleDetailController extends Controller
+{
+    public function detail(int $id)
+    {
+        try{
+            
+            $user = user::getUserInfoMax4ExcludeMe($id)->first();
+            if (is_null($user)) {
+                return response()->json([
+                    'success' => false,
+                    'code' => 404,
+                    'message' => "お探しのページが見つからなかったよ。"//$e,
+                ], 404);
+            }
+            return response()->json([
+                'success' => true,
+                'code' => 200,
+                'data' => 
+                        [
+                            'name' => $user->name,
+                            'email' => $user->email,
+                        ]
+            ],200);
+        }catch(Exception $e){
+            throw new HttpResponseException(response()->json([
+                'success' => false,
+                'code' => 500,
+                'message' => "問題が発生しちゃったよ"//$e,
+            ], 500));
+        }
+
+
+        $user = user::getUserInfoMax4ExcludeMe($id)->first();
+        if (is_null($user)) {
+            throw new BadRequestException();
+        }
+        return view('user.user_detail', ['user' => $user]);
+    }
+
+}
